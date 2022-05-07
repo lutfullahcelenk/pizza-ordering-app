f22bad34e16139818566705f22e9d9b97b48e034
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-05-08 00:45:07 +0300

    Navbar section is completed responsively

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Featured from '../components/Features'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      homepage
+      <Featured />
 
     </div>
   )
