8228ac015adf2f9eed0c932d82143a4970807ff9
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-05-08 02:46:39 +0300

    the amounts are updated

--- pages/Cart.jsx
+++ pages/Cart.jsx
@@ -34,13 +34,13 @@ const Cart = () => {
               </span>
             </td>
             <td>
-              <span className={styles.price}>$19.90</span>
+              <span className={styles.price}>$29.90</span>
             </td>
             <td>
               <span className={styles.quantity}>2</span>
             </td>
             <td>
-              <span className={styles.total}>$39.80</span>
+              <span className={styles.total}>$59.80</span>
             </td>
           </tr>
           <tr className={styles.tr}>
@@ -76,15 +76,15 @@ const Cart = () => {
       </div>
       <div className={styles.right}>
         <div className={styles.wrapper}>
-          <h2 className={styles.title}>CART TOTAL</h2>
+          <h2 className={styles.title}>CARD TOTAL</h2>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+            <b className={styles.totalTextTitle}>Subtotal:</b>$99.60
           </div>
           <div className={styles.totalText}>
             <b className={styles.totalTextTitle}>Discount:</b>$0.00
           </div>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Total:</b>$79.60
+            <b className={styles.totalTextTitle}>Total:</b>$99.60
           </div>
           <button className={styles.button}>CHECKOUT NOW!</button>
         </div>
